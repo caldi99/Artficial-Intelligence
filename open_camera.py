991e43dfb5a8d19a1bbdeefca093c9c7c2d14d79
caldi_99 <francesco.caldivezzi@hotmail.it>
2022-03-10 10:35:42 +0100

    .gitignore is now working

--- open_camera.py
+++ open_camera.py
@@ -8,7 +8,13 @@ if not cap.isOpened():
 
 while True:
     ret, frame = cap.read()
+    
+    
     frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    
+    
+    
+    
     cv2.imshow('Input', frame)
 
     c = cv2.waitKey(1)
